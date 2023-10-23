0703588bd9240e94d4ac2be464206e53cffc98b3
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2023-10-23 16:12:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikumregistrasi1/Data/DataForm.kt
@@ -0,0 +1,5 @@
+package com.example.praktikumregistrasi1.Data
+
+data class DataForm(
+    val sex: String = ""
+)
---
+++ app/src/main/java/com/example/praktikumregistrasi1/Data/DataSource.kt
@@ -0,0 +1,10 @@
+package com.example.praktikumregistrasi1.Data
+
+import com.example.praktikumregistrasi1.R
+
+object DataSource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
